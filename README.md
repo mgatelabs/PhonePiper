30b3e816aa100e3219897211d8b1561d475026e9
Michael Fuller <mfuller@calliduscloud.com>
2017-08-30 08:21:36 -0500

    Structure Changes
    1. Updated readme
    2. Runner uses a better format for running scripts, because in the future I assume more options will become available.
    3. Defaults to the Axon7 (axon7) phone

--- README.md
+++ README.md
@@ -6,10 +6,24 @@ You will need android studio and android debgging tools in order for this to fun
 
 This is currently built for my phone, the Axon7 and will not work with any other device.
 
-# What is it doing
+## Example
+
+0. Have a Axon7 phone & Android Studio with Debug tools.  Make sure you phone has developer options enabled.
+1. Setup a working directory
+2. Open FFBE and navigate to the Earth Shrine mission list.
+3. run with the arguments "run run earthshrine-exit"
+
+## What is it doing
 
 1. Grabbing the current screen
 2. Loading the screen into memory
 3. Iterate through all available actions, for the current phase
 4. For each action check if it should execute by comparing pixels to known values
 5. Execute the command and move forward
+
+## What needs to happen
+
+1. Map parsing
+2. Better action handling with branches, pushing, popping switching
+3. Implement swipes
+4. Better way to analyze screens for initial setup, maybe with electron
--- src/main/java/com/mgatelabs/ffbe/Runner.java
+++ src/main/java/com/mgatelabs/ffbe/Runner.java
@@ -6,24 +6,44 @@ import com.mgatelabs.ffbe.shared.*;
  * Created by @mgatelabs (Michael Fuller) on 8/27/2017.
  */
 public class Runner {
-    public static void main(String [] args) {
-        GameRunner runner = new GameRunner();
+  public static void main(String[] args) {
+    GameRunner runner = new GameRunner();
 
-        if (args.length == 1 && args[0].equals("snap")) {
-            runner.snap();
-            return;
-        } else if (args.length != 2) {
-            System.out.println("Required Parameters: phone script");
-            return;
-        }
 
-        Phone phone = runner.loadPhone(args[0]);
+    boolean showHelp = false;
 
-        Script script = runner.loadScript(args[1]);
+    if (args.length >= 1) {
+      if ("snap".equalsIgnoreCase(args[0])) {
+        runner.snap();
+        return;
+      } else if ("run".equalsIgnoreCase(args[0])) {
+        if (args.length < 2) {
+          showHelp = true;
+        } else {
+          final String scriptName = args[1];
+          final String phoneName = args.length == 2 ? "axon7" : args[2];
 
-        if (phone != null && script != null) {
+          Script script = runner.loadScript(scriptName);
+
+          Phone phone = runner.loadPhone(phoneName);
+
+          if (phone != null && script != null) {
             runner.run(phone, script);
+          } else if (phone == null) {
+            System.out.println("Error: could not find phone with name " + phoneName);
+          } else {
+            System.out.println("Error: could not find script with name " + scriptName);
+          }
         }
-        //runner.earthShrine();
+      }
+    } else {
+      showHelp = true;
     }
+
+    if (showHelp) {
+      System.out.println("Required Parameters missing");
+      System.out.println("run (scriptName) [phoneName]");
+      System.out.println("snap");
+    }
+  }
 }

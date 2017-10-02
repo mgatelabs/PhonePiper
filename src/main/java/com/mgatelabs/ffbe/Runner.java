890fc5de6aab6fa2b1e09b89443c29652f7d6e5a
Michael Fuller <mfuller@calliduscloud.com>
2017-10-02 14:38:04 -0500

    Fixes
    1. Runner cleanup, it now also defaults to gui
    2. Made a 1 and 2 unit version of lb farming

--- src/main/java/com/mgatelabs/ffbe/Runner.java
+++ src/main/java/com/mgatelabs/ffbe/Runner.java
@@ -19,10 +19,67 @@ import java.util.regex.Pattern;
 public class Runner {
     public static void main(String[] args) {
 
-
         boolean showHelp = false;
 
-        if (args.length >= 1) {
+        if ((args.length >= 1 && "gui".equalsIgnoreCase(args[0])) || args.length == 0) {
+
+            PlayerDefinition playerDefinition = PlayerDefinition.read();
+
+            final ImageIcon imageIcon = new ImageIcon(playerDefinition.getClass().getResource("/icon.png"));
+
+            while (true) {
+                StartupFrame startupFrame = new StartupFrame(playerDefinition, imageIcon);
+
+                while (startupFrame.isShowing()) {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                if (startupFrame.getSelectedAction() != null && startupFrame.getSelectedMode() != null) {
+
+                    FrameChoices frameChoices = new FrameChoices(startupFrame.getSelectedAction(), startupFrame.getSelectedMode(), playerDefinition, startupFrame.getSelectedMap(), startupFrame.getSelectedScript(), startupFrame.getSelectedDevice(), startupFrame.getSelectedView());
+
+                    if (frameChoices.getAction() == FrameChoices.Action.CREATE) {
+                        String inputValue = JOptionPane.showInputDialog("Please input a " + frameChoices.getMode().name());
+                        if (inputValue == null || inputValue.trim().length() == 0) {
+                            continue;
+                        } else if (!Constants.ID_PATTERN.matcher(inputValue).matches()) {
+                            JOptionPane.showMessageDialog(null, "Invalid string format, only allow a-z A-Z 0-9 - _");
+                            continue;
+                        } else if (!frameChoices.canCreate(inputValue)) {
+                            JOptionPane.showMessageDialog(null, "Invalid name.  File may already exist.");
+                            continue;
+                        }
+                    }
+
+                    if (frameChoices.isValid()) {
+                        MainFrame frame = new MainFrame(frameChoices, imageIcon);
+
+                        while (frame.isShowing()) {
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        if (!frame.isReturnRequested()) {
+                            break;
+                        }
+                    } else {
+
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            System.exit(0);
+            //new MainFrame().setVisible(true);
+        } else if (args.length >= 1) {
             if ("manage".equalsIgnoreCase(args[0]) || "manager".equalsIgnoreCase(args[0])) {
                 DeviceDefinition deviceDefinition = null;
                 if (args.length == 2) {
@@ -37,64 +94,6 @@ public class Runner {
                 GameManager manager = new GameManager(deviceDefinition);
 
                 manager.manage();
-            } else if ("gui".equalsIgnoreCase(args[0])) {
-
-                PlayerDefinition playerDefinition = PlayerDefinition.read();
-
-                final ImageIcon imageIcon = new ImageIcon(playerDefinition.getClass().getResource("/icon.png"));
-
-                while (true) {
-                    StartupFrame startupFrame = new StartupFrame(playerDefinition, imageIcon);
-
-                    while (startupFrame.isShowing()) {
-                        try {
-                            Thread.sleep(100);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if (startupFrame.getSelectedAction() != null && startupFrame.getSelectedMode() != null) {
-
-                        FrameChoices frameChoices = new FrameChoices(startupFrame.getSelectedAction(), startupFrame.getSelectedMode(), playerDefinition, startupFrame.getSelectedMap(), startupFrame.getSelectedScript(), startupFrame.getSelectedDevice(), startupFrame.getSelectedView());
-
-                        if (frameChoices.getAction() == FrameChoices.Action.CREATE) {
-                            String inputValue = JOptionPane.showInputDialog("Please input a " + frameChoices.getMode().name());
-                            if (inputValue == null || inputValue.trim().length() == 0) {
-                                continue;
-                            } else if (!Constants.ID_PATTERN.matcher(inputValue).matches()) {
-                                JOptionPane.showMessageDialog(null, "Invalid string format, only allow a-z A-Z 0-9 - _");
-                                continue;
-                            } else if (!frameChoices.canCreate(inputValue)) {
-                                JOptionPane.showMessageDialog(null, "Invalid name.  File may already exist.");
-                                continue;
-                            }
-                        }
-
-                        if (frameChoices.isValid()) {
-                            MainFrame frame = new MainFrame(frameChoices, imageIcon);
-
-                            while (frame.isShowing()) {
-                                try {
-                                    Thread.sleep(100);
-                                } catch (InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-
-                            if (!frame.isReturnRequested()) {
-                                break;
-                            }
-                        } else {
-
-                        }
-                    } else {
-                        break;
-                    }
-                }
-
-                System.exit(0);
-                //new MainFrame().setVisible(true);
             } else if ("frame".equalsIgnoreCase(args[0])) {
 
                 long startTime = System.nanoTime();
@@ -105,88 +104,7 @@ public class Runner {
 
                 System.out.println("Frame captured in: " + ((float) dif / 1000000000.0) + "s");
 
-        /*
-        File sampleFile = new File("pieces//friendrequest.png");
-
-        BufferedImage bufferedImage = null;
-
-        try {
-          bufferedImage = ImageIO.read(sampleFile);
-        } catch (IOException e) {
-          e.printStackTrace();
-          return;
-        }
-
-        ImageWrapper rawImageWrapper = new PngImageWrapper(bufferedImage);// GameRunner.getScreen();
-
-        ImagePixelPickerDialog dialog = new ImagePixelPickerDialog(ImagePixelPickerDialog.Mode.BOX);
-        dialog.setup(rawImageWrapper, new ArrayList<>());
-        dialog.start();
-      */
-
-                //runner.snap();
                 return;
-            } /*else if ("mapper".equalsIgnoreCase(args[0])) {
-        final String phoneName = args.length == 1 ? "axon7" : args[2];
-        Phone phone = runner.loadPhone(phoneName);
-        DungeonMapperCommandLine dungeonMapperCommandLine = new DungeonMapperCommandLine(phone);
-        dungeonMapperCommandLine.run();
-        return;
-      }*/ else if ("script".equalsIgnoreCase(args[0])) {
-
-                if (args.length < 2) {
-                    showHelp = true;
-                } else {
-                    final String scriptName = args[1];
-                    final String deviceName = args.length == 2 ? "axon7" : args[2];
-
-                    ScriptDefinition script = ScriptDefinition.read(scriptName);
-                    if (script == null) {
-                        System.out.println("Could not locate script: " + scriptName);
-                        return;
-                    }
-                    DeviceDefinition device = DeviceDefinition.read(deviceName);
-                    if (device == null) {
-                        System.out.println("Could not locate device: " + deviceName);
-                        return;
-                    }
-                    ViewDefinition view = ViewDefinition.read(device.getViewId());
-                    if (view == null) {
-                        System.out.println("Could not locate view: " + device.getViewId());
-                        return;
-                    }
-                    ConnectionDefinition connectionDefinition = ConnectionDefinition.read();
-                    PlayerDefinition playerDefinition = PlayerDefinition.read();
-                    if (playerDefinition == null) {
-                        playerDefinition = new PlayerDefinition();
-                        while (true) {
-                            System.out.println("Please enter your current Player Level: ");
-                            int level = ConsoleInput.getInt();
-                            if (level > 8 && level <= 150) {
-                                playerDefinition.setLevel(level);
-                                playerDefinition.write();
-                                break;
-                            } else {
-                                System.out.println("Invalid level, it must be between 8 and 150.  Stopping.");
-                                return;
-                            }
-                        }
-
-                    }
-                    //ScriptRunner scriptRunner = new ScriptRunner(playerDetail, connectionDefinition, script, device, view);
-
-                    System.out.println("----------");
-                    System.out.println("Script: " + scriptName);
-                    System.out.println("Device: " + deviceName);
-                    System.out.println("View: " + device.getViewId());
-                    System.out.println("Energy: " + playerDefinition.getTotalEnergy());
-                    System.out.println("----------");
-                    System.out.println();
-
-                    //scriptRunner.run("main");
-                }
-
-
             }
         } else {
             showHelp = true;

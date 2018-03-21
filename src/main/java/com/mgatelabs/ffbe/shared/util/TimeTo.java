5f967982221e7774c97dbb4db213e0c7c9643e9b
Michael Fuller <mmgate@hotmail.com>
2018-03-20 23:46:57 -0500

    Updates
    1. Changed Timer format

---
+++ src/main/java/com/mgatelabs/ffbe/shared/util/TimeTo.java
@@ -0,0 +1,22 @@
+package com.mgatelabs.ffbe.shared.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by @mgatelabs (Michael Fuller) on 3/20/2018.
+ */
+public class TimeTo {
+
+    public static int MINUTES = 60;
+    public static int HOURS = 60 * 60;
+
+    public static final SimpleDateFormat SDF = new SimpleDateFormat("HH:mm:ss");
+
+    public static String format(double seconds) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2000, 1,1,0,0,0);
+        calendar.add(Calendar.SECOND, (int) seconds);
+        return SDF.format(calendar.getTime());
+    }
+}
--- src/main/java/com/mgatelabs/ffbe/shared/util/VarTimer.java
+++ src/main/java/com/mgatelabs/ffbe/shared/util/VarTimer.java
@@ -33,7 +33,7 @@ public class VarTimer {
         } else {
             float lastDiff = ((float) (end - last) / 1000000000.0f);
             float avgDiff = ((float) (end - start) / 1000000000.0f) / count;
-            return "Time: " + String.format("%2.2f", lastDiff)+ "s Average: " + String.format("%2.2f", avgDiff) + "s";
+            return "Time: " + TimeTo.format(lastDiff)+ " Average: " + TimeTo.format(avgDiff);
         }
     }
 }

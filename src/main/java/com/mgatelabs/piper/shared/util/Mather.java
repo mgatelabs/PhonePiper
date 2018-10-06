f2e062a785cefd359f50139df5e528e94bedfdf4
Michael Fuller <mgatelabs@users.noreply.github.com>
2018-10-06 14:46:21 -0500

    0.0.13.1 Update (#6)
    1. Added RANDOM action
    2. New files _common-hp & _common-mp
    3. Updated raid script to steal gil with XON
    4. Math fixes, because subtraction was weird
    5. More view components

--- src/main/java/com/mgatelabs/piper/shared/util/Mather.java
+++ src/main/java/com/mgatelabs/piper/shared/util/Mather.java
@@ -43,30 +43,12 @@ public class Mather {
         }
 
         if (input.contains("/")) {
-            int multiplyIndex = input.indexOf('/');
-            String left = input.substring(0, multiplyIndex);
-            String right = input.substring(multiplyIndex + 1);
+            int divideIndex = input.indexOf('/');
+            String left = input.substring(0, divideIndex);
+            String right = input.substring(divideIndex + 1);
             Var l = evaluate(left, expectedType);
             Var r = evaluate(right, expectedType);
             return l.divide(r);
-        }
-
-        if (input.contains("+")) {
-            int multiplyIndex = input.indexOf('+');
-            String left = input.substring(0, multiplyIndex);
-            String right = input.substring(multiplyIndex + 1);
-            Var l = evaluate(left, expectedType);
-            Var r = evaluate(right, expectedType);
-            return l.add(r);
-        }
-
-        if (input.contains("-")) {
-            int multiplyIndex = input.indexOf('-');
-            String left = input.substring(0, multiplyIndex);
-            String right = input.substring(multiplyIndex + 1);
-            Var l = evaluate(left, expectedType);
-            Var r = evaluate(right, expectedType);
-            return l.substract(r);
         }
 
         if (input.contains("%")) {
@@ -76,6 +58,43 @@ public class Mather {
             Var l = evaluate(left, expectedType);
             Var r = evaluate(right, expectedType);
             return l.mod(r);
+        }
+
+        if (input.contains("+")) {
+            int plusIndex = input.indexOf('+');
+            String left = input.substring(0, plusIndex);
+            String right = input.substring(plusIndex + 1);
+            Var l = evaluate(left, expectedType);
+            Var r = evaluate(right, expectedType);
+            return l.add(r);
+        }
+
+        if (input.contains("-")) {
+            int subtractIndex = input.indexOf('-');
+            String left = input.substring(0, subtractIndex);
+            String right = input.substring(subtractIndex + 1);
+            Var l = evaluate(left, expectedType);
+            int otherIndexPlus = right.indexOf('+');
+            int otherIndexMinus = right.indexOf('-');
+            if (otherIndexMinus == -1 && otherIndexPlus == -1) {
+                Var r = evaluate(right, expectedType);
+                return l.substract(r);
+            } else {
+                int min = Integer.min(otherIndexMinus, otherIndexPlus);
+                int max = Integer.max(otherIndexMinus, otherIndexPlus);
+                String right1;
+                String right2;
+                if (min == -1) {
+                    right1 = right.substring(0, max);
+                    right2 = right.substring(max);
+                } else {
+                    right1 = right.substring(min);
+                    right2 = right.substring(min);
+                }
+                Var m = evaluate(right1, expectedType);
+                Var e = evaluate(right2, expectedType);
+                return l.substract(m).add(e);
+            }
         }
 
         input = input.trim();

c2e9bc6b4011225ab26c48b7fac642c7458d5ace
Michael Fuller <mmgate@hotmail.com>
2018-10-16 15:10:47 -0500

    0.0.16.7
    1. Removed GUI
    2. Updated HP/MP functions
    3. Updated Ayaka scripts

--- src/main/java/com/mgatelabs/piper/ui/utils/Constants.java
+++ src/main/java/com/mgatelabs/piper/ui/utils/Constants.java
@@ -1,5 +1,13 @@
 package com.mgatelabs.piper.ui.utils;
 
+import com.google.common.collect.Lists;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 /**
@@ -19,4 +27,55 @@ public class Constants {
     public static final String MODE_VIEW = "view";
 
     public static final Pattern ID_PATTERN = Pattern.compile("^[a-zA-Z0-9-_]+$");
+    public static final String PATH_DEVICES = "./devices";
+    public static final String PATH_VIEWS = "./views";
+    public static final String PATH_SCRIPTS = "./scripts";
+
+    public static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    public static String nullToEmpty(String s) {
+        return s == null ? "" : s;
+    }
+
+    public static String[] listJsonFilesIn(File dir) {
+        List<String> itemList = Lists.newArrayList();
+        itemList.add("");
+        for (File f : dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".json") && !name.startsWith("_");
+            }
+        })) {
+            itemList.add(f.getName().substring(0, f.getName().length() - 5));
+        }
+        Collections.sort(itemList);
+        String[] itemArray = new String[itemList.size()];
+        itemList.toArray(itemArray);
+        return itemArray;
+    }
+
+    public static String[] listFoldersFilesIn(File dir) {
+        List<String> itemList = Lists.newArrayList();
+        itemList.add("");
+        for (File f : dir.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.isDirectory();
+            }
+        })) {
+            itemList.add(f.getName());
+        }
+        Collections.sort(itemList);
+        String[] itemArray = new String[itemList.size()];
+        itemList.toArray(itemArray);
+        return itemArray;
+    }
+
+    public static List<String> arrayToList(String[] items) {
+        List<String> list = Lists.newArrayList();
+        for (String item : items) {
+            list.add(item);
+        }
+        return list;
+    }
 }

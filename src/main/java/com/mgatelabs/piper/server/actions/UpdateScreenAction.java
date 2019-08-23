6c3767c323c1d2cb807be18c4f29dc55b255f225
Michael Fuller <developer@mgatelabs.com>
2019-08-22 22:28:38 -0500

    2019.8
    1. Edit fixes

--- src/main/java/com/mgatelabs/piper/server/actions/UpdateScreenAction.java
+++ src/main/java/com/mgatelabs/piper/server/actions/UpdateScreenAction.java
@@ -15,7 +15,7 @@ public class UpdateScreenAction implements EditActionInterface {
     public String execute(final String id, final String value, final EditHolder holder) {
         ScreenDefinition screenDefinition = holder.getScreenForId(id);
         if (screenDefinition == null) return "Could not find screen with id: " + id;
-        AdbUtils.persistScreen(holder.getShell());
+        holder.getDeviceHelper().refresh(holder.getShell());
         ImageWrapper wrapper = holder.getDeviceHelper().download();
         if (wrapper != null && wrapper.isReady()) {
             File previewPath = ScreenDefinition.getPreviewPath(holder.getViewDefinition().getViewId(), screenDefinition.getScreenId());
